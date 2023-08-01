5595b1ff9cdf33f94991381e006beb9f852b7b06
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-08-01 20:06:57 +0200

    install eslint, add models files

---
+++ bin/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+    },
+    "extends": "airbnb-base",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+  }

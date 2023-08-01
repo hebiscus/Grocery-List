ab89709a52e1717cc16939ab0185638daf955a66
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-08-01 20:50:32 +0200

    add product category schema

---
+++ models/productCategory.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductCategorySchema = new Schema({
+    name: {type: String, required: true, minLength: 3, maxLength: 15},
+    description: {type: String, maxLength: 500},
+    commonExamples: {type: Array},
+})
+
+ProductCategorySchema.virtual("url").get(function () {
+    return `/categories/${this.id}`;
+})
+
+module.exports = mongoose.model("ProductCategory", ProductCategorySchema);

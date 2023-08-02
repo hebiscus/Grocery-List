8e44fa46ecebdfa2e8a31bb224cdb454d3149d75
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-08-02 19:38:01 +0200

    add main routes, redirects on main

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/', function(req, res, next) {
+  res.redirect('/categories')
+});
+
+router.get('/', function(req, res, next) {
+  res.redirect('/list')
+});
+
+router.get('/', function(req, res, next) {
+  res.redirect('/stores')
+});
+
 module.exports = router;
